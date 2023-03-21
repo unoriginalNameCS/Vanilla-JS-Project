d445b240713e3a95d7471f45136d6ca9e6bed21b
Jeshnil Kumar <j.kumar@student.unsw.edu.au>
2023-03-21 23:20:30 +1100

    changes to frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,3 +3,36 @@ import { BACKEND_PORT } from './config.js';
 import { fileToDataUrl } from './helpers.js';
 
 console.log('Let\'s go!');
+
+document.getElementById("register-btn").addEventListener("click", () => {
+    const registerEmail = document.getElementById("register-email").value;
+    const registerPassword = document.getElementById("register-password").value;
+    const registerName = document.getElementById("register-first-name").value;
+    
+    console.log(registerEmail, registerPassword, registerName);
+    
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',        
+        },
+        body: JSON.stringify({
+            email: registerEmail,
+            password: registerEmail,
+            name: registerName,
+        }),
+    }
+
+    fetch("http://localhost:5005/auth/register", options)
+        .then((res) => {
+            res.json()
+                .then((data) => {
+                    if (data.error) {
+                        alert(data.error);
+                    } else {
+                        console.log("Success", data);
+                    }
+                });
+            console.log("I got the response! ", res);
+        });
+});
